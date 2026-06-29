8c6b3d7ab39b2118f92fc6b711eff10ed40bc5ce
notthatjezus <jezuschez@gmail.com>
2026-06-29 20:10:52 +0300

    add simple program

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+import random as r
+import time
+
 toggle_state = "off"
 
 def toggle():
@@ -6,5 +9,26 @@ def toggle():
     return toggle_state
         
         
-print(toggle())
-print(toggle())
+def random_num():
+    num = r.choice(range(0, 1000))
+    return num
+
+while True:
+    choice = int(input("Make your choice:\n"\
+                   "1. Switch toggle\n"\
+                   "2. Randon num\n"\
+                   "Press any key to exit\n",
+                   ))
+    print()
+    
+    if choice == 1:
+        print(f'Switch {toggle()}', end="\n\n")
+        time.sleep(1)
+        
+    elif choice == 2:
+        print(f'Your random num is {random_num()}', end="\n\n")
+        time.sleep(1)
+        
+    else:
+        print("Bye!")
+        break
