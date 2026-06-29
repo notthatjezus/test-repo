0f884063b29cd64cf7ac60dca6a235783d37dc12
notthatjezus <jezuschez@gmail.com>
2026-06-29 19:09:18 +0300

    first test commit

---
+++ practice.py
@@ -0,0 +1,14 @@
+def to_switch_tongle():
+    tongle = ""
+    if tongle == True:
+        print("switch off")
+        tongle = False
+    else:
+        print("switch on")
+        tongle = True
+        
+
+
+to_switch_tongle()
+to_switch_tongle()
+to_switch_tongle()
